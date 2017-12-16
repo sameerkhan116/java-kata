073281910bffa67e091e87eecf96c38e1cca6ab8
Sameer Khan <sameer_khan@student.uml.edu>
2017-12-16 15:49:31 -0500

    Loved this one

--- JS/chooseBestsum.js
+++ JS/chooseBestsum.js
@@ -1,8 +1,16 @@
 function chooseBestSum(t, k, ls) {
-    for(var i = 0; i < ls.length; i++) {
-
+    function combinations(ls, k) {
+      return k == 0 ? [[]] : ls.map(function (e, i) {
+        return combinations(ls.slice(i+1), k-1).map(function (c) {
+          return [e].concat(c);
+        });
+      }).reduce(function (a, b) {
+        return a.concat(b);
+      }, []);
     }
+    var distances = combinations(ls, k).map(c => c.reduce((e, a) => e + a));
+    return distances.filter(e => e <= t).sort((a, b) => b - a)[0] || null;
 }
 
-ts = [50, 55, 56, 57, 58];
-console.log(chooseBestSum(163, 3, ts))
+ts = [91, 74, 73, 85, 73, 81, 87]
+console.log(chooseBestSum(230, 3, ts))
